3c0797be8ca03a1a2cfe1912855253a75768a8bc
devKiratu <dev.kiratu@gmail.com>
2023-01-12 14:11:10 +0300

    set up typography, color pallete and background

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,26 @@
 
 <template>
   <h1>Hello world</h1>
+  <h2>Hello world</h2>
+  <h3>Hello world</h3>
+  <h4>Hello world</h4>
+  <h5>Hello world</h5>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  color: var(--pale-blue);
+}
+h2 {
+  color: var(--bright-blue);
+}
+h3 {
+  color: var(--very-pale-blue);
+}
+h4 {
+  color: var(--desaturated-blue);
+}
+h5 {
+  color: var(--dark-blue);
+}
+</style>
